23a284570c3741cc96a16648e422a9e8101d7f37
GokulsreeY <Siri@Gokulsrees-MacBook-Pro.local>
2017-02-09 20:13:54 -0500

    Added loading
    Added pull to refresh and infinite scroll

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 import AFNetworking
+import SVPullToRefresh
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var isMoreDataLoading = false
+    var offset = 0
   var posts: [NSDictionary] = []
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -16,36 +19,47 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         
         tableView.delegate = self
         tableView.dataSource = self
-        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
-        let request = URLRequest(url: url!)
-        let session = URLSession(
-            configuration: URLSessionConfiguration.default,
-            delegate:nil,
-            delegateQueue:OperationQueue.main
-        )
+//        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+//        let request = URLRequest(url: url!)
+//        let session = URLSession(
+//            configuration: URLSessionConfiguration.default,
+//            delegate:nil,
+//            delegateQueue:OperationQueue.main
+//        )
+//        
+//        let task : URLSessionDataTask = session.dataTask(
+//            with: request as URLRequest,
+//            completionHandler: { (data, response, error) in
+//                if let data = data {
+//                    if let responseDictionary = try! JSONSerialization.jsonObject(
+//                        with: data, options:[]) as? NSDictionary {
+//                        print("responseDictionary: \(responseDictionary)")
+//                        
+//                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+//                        // This is how we get the 'response' field
+//                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+//                               self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+//                        
+//                        // This is where you will store the returned array of posts in your posts property
+//                        // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+//                        self.tableView.reloadData()
+//                    }
+//                    
+//                }
+//        });
+//        task.resume()
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action:  #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        loadMoreData()
         
-        let task : URLSessionDataTask = session.dataTask(
-            with: request as URLRequest,
-            completionHandler: { (data, response, error) in
-                if let data = data {
-                    if let responseDictionary = try! JSONSerialization.jsonObject(
-                        with: data, options:[]) as? NSDictionary {
-                        print("responseDictionary: \(responseDictionary)")
-                        
-                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
-                        // This is how we get the 'response' field
-                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
-                               self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
-                        
-                        // This is where you will store the returned array of posts in your posts property
-                        // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
-                        self.tableView.reloadData()
-                    }
-                    
-                }
-        });
-        task.resume()
+    
+        
+        
+        
         // Do any additional setup after loading the view.
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -63,7 +77,9 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
+        
+       
+    let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
         
         let post = posts[indexPath.row]
           
@@ -84,6 +100,12 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         else{
             
         }
+       
+        tableView.addInfiniteScrolling(){
+            self.tableView.reloadData()
+            //self.tableView.infiniteScrollingView?.stopAnimating()
+            self.tableView.indexPath(for: cell)
+        }
         
         tableView.rowHeight = 240
         
@@ -93,7 +115,87 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
         
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! PhotoCell
+        var destinationViewController = segue.destination as! PhotoDetailsViewController
+        
+        destinationViewController.imageTouse = cell.pictureView.image
+    
+    }
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        //let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        //let request = URLRequest(url: url!)
+        // ... Create the URLRequest `myRequest` ...
+        let request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        
+        // Configure session so that completion handler is executed on main UI thread
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            
+            // ... Use the new data to update the data source ...
+            
+            // Reload the tableView now that there is new data
+            self.tableView.reloadData()
+            
+            // Tell the refreshControl to stop spinning
+            refreshControl.endRefreshing()
+        }
+        task.resume()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if(!isMoreDataLoading){
+            isMoreDataLoading = true
+        }
+        let scrollViewContentHeight = tableView.contentSize.height
+        let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+        
+        // When the user has scrolled past the threshold, start requesting
+        if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
+            isMoreDataLoading = true
+            offset += 1
+            loadMoreData()
+            // ... Code to load more results ...
+        }
+        // Handle scroll behavior here
+    }
+    
+    func loadMoreData(){
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV&offset=\(offset*self.posts.count)")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        self.posts += responseFieldDictionary["posts"] as! [NSDictionary]
+                        
+                        // This is where you will store the returned array of posts in your posts property
+                        // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+                        self.tableView.reloadData()
+                        
+                    }
+                    
+                }
+                self.isMoreDataLoading = false
+        });
+        task.resume()
+        
+    }
     /*
     // MARK: - Navigation
 
